73311fccfb22c7d1370b2c65f210e5b75611d543
Joao19822 <jv261555@gmail.com>
2024-06-21 22:01:19 -0300

    Array
    Array testado

--- array.js
+++ array.js
@@ -1,14 +1,31 @@
-var btn = document.getElementById
-('cadastar')
 var alunos = [];
 
-btn.addEventListener('click' ,e=>{
-    let nome =document.getElementById
-    ('nome')
-    alunos.push(nome.value)
-    nome.value = ""
-    console.log(alunos)
+function listar_alunos() {
+    let listar = document.getElementById
+        ('listar')
+    listar.innerHTML = "";
+    alunos.forEach(aluno => {
+        listar.innerHTML += `<li>${aluno} <button>Editar</button><button>Excluir</button</li>`;
+    });
+}
+listar_alunos();
+
+var btn = document.getElementById('cadastar')
+
+btn.addEventListener('click', e => {
+    let nome = document.getElementById
+        ('nome')
+        if (nome.value == "") {
+            alert('digita algo pelo menos😑')
+        } else {
+            alunos.push(nome.value)
+            nome.value = ""
+            listar_alunos();
+
+        }
+
     
+
 })
 
-console.log(alunos)
+
